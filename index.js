55e01632dc64f079164cd8613db016ca00ffefb7
EleazarSauz <sauz@nueracorestudio.com>
2019-03-06 19:25:58 -0600

    chat

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const  express = require('express');
+const SocketIO = require('socket.io');
 
 const app = express();
 
 
 // RUTAS
-app.get('/', (req, res) => res.send('holi'));
-
+app.get('/', (req, res) => res.send('holi :)'));
+app.get('/chat', (req, res) => res.sendFile(__dirname + '/chat.html') );
+    
 // CONFIGRURACIÓN DEL PUERTO
 app.set('port', process.env.PORT || 8888);  // determinar el puerto por defecto o selecionar el 8888
 const port = app.get('port') // guardar en una costante
 
-app.listen(port, () => {
-    console.log('Servidor corriendo en el puerto', port, ', enjoy! :D') // levantar el servidor
+const server = app.listen(port, () => {   // levantar el servidor y guardar en una constante
+    console.log('Servidor corriendo en el puerto', port, ', enjoy! :D') 
+});
+
+app.use(express.static)
+
+// WEBSOCKETS
+const io = SocketIO(server);
+
+io.on('connection', function (socket) {
+    console.log('Un usuario se conecto :o');
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
 });
